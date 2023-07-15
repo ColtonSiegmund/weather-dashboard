56f4c10fc1a569286e9514e2da5265d7802bd16a
ColtonSiegmund <135175571+ColtonSiegmund@users.noreply.github.com>
2023-07-15 10:51:18 -0500

    added javascript functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-
-$('.inputBtn').on('click', function() {
-
+// grab user input
+// concat API link
+var cityInput = document.querySelector(".inputText")
+var weatherContainer = document.querySelector('.weather')
 var inputValue = $('.inputTxt').val();
-localStorage.setItem("inputtext", inputValue);
-console.log(inputValue);
-});
+
+
+
+function getLatLong () {
+var inputValue = $('.inputTxt').val();
+var apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputValue + '&limit=1&appid=633f2ef3a054cf44fe6adc7f6717f94c';
+fetch(apiURL)
+.then(function (response){
+  return response.json();
+})
+.then(function (data){
+  for(var i=0; i< data.length; i++){
+  var latInput = (data[i].lat);
+  var lonInput = (data[i].lon);
+  };
+})
+.then(function (fetch){
+  var weatherURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + latInput + '&lon=' + data[i].lon + '&appid=633f2ef3a054cf44fe6adc7f6717f94c';
+})
+};
+
+function getWeather() {
+  var weatherURL = 'api.openweathermap.org/data/2.5/forecast?lat=' + latInput + '&lon=' + data[i].lon + '&appid=633f2ef3a054cf44fe6adc7f6717f94c';
+  fetch (weatherURL)
+  .then(function(response){
+    return response.json();
+})
+console.log(weatherURL)
+};
+
+$('.inputBtn').on('click', getLatLong)
 
